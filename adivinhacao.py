cda5db7824a5835ea97e7c1233567c24e771f281
mateusflip <mateus.ramos1337@gmail.com>
2020-03-07 22:57:55 -0300

    Gerando número aleatório e FOR

--- adivinhacao.py
+++ adivinhacao.py
@@ -1,14 +1,16 @@
 total_tentativas = 3
-rodada = 1
+import random
+numero_adivinhacao = random.randrange(1, 101) #Gera um número aleatório de 1 a 100, lembrando que ele exclui o ultimo parametro que é o 101#
+print("o número da adivinhação é ", numero_adivinhacao)
 
-while(rodada <= total_tentativas):
-  numero_adivinhacao = 34
-
+for rodada in range(1, total_tentativas + 1): #o for nao reconhe o ultimo termo, por isso temos que comar mais um#
   print("Essa é a sua tentativa {} de {}".format(rodada, total_tentativas))
-
-  numero = int(input("Digite um número para acertar "))
+  numero = int(input("Digite um número de 1 a 100 "))
 
   print("Seu número é ", numero)
+  if (numero < 1 or numero > 100):
+      print("Numero inválido, você deve digitar um número de 1 a 100")
+      continue # Encerra a interação do laço indo pro proximo#
 
   acertou = (numero == numero_adivinhacao)
   menor = (numero < numero_adivinhacao)
@@ -16,10 +18,10 @@ while(rodada <= total_tentativas):
 
   if (acertou):
     print("Você acertou")
+    break
   else:
       if(menor):
         print("Você errou, o número é maior")
       elif(maior):
         print("Você errou, o número é menor")
 
-  rodada = rodada + 1
